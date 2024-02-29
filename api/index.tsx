8f28f58ea55116953be175920a6c3840f46bf70c
Jasper Dansercoer <jasper@codictive.be>
2024-02-29 20:39:44 +0100

    Initial commit

---
+++ api/index.tsx
@@ -0,0 +1,19 @@
+import { Frog } from "frog";
+import { handle } from "frog/vercel";
+import { frame } from "./Frame.js";
+
+// Uncomment to use Edge Runtime.
+// export const config = {
+//   runtime: 'edge',
+// }
+
+export const app = new Frog({
+  basePath: "/api",
+  // Supply a Hub API URL to enable frame verification.
+  // hubApiUrl: 'https://api.hub.wevm.dev',
+});
+
+frame();
+
+export const GET = handle(app);
+export const POST = handle(app);
